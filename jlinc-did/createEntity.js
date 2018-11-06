e46055ad19703725a029856bf750bba7c02cd9ef
Victor Grey <victorgrey@users.noreply.github.com>
2018-11-05 22:23:53 -0800

    Use separate keys for encrypting instead of deriving them from the signing keys.

--- jlinc-did/createEntity.js
+++ jlinc-did/createEntity.js
@@ -5,15 +5,14 @@ const sodium = require('sodium').api;
 
 module.exports = function createEntity(){
   const { publicKey, secretKey } = sodium.crypto_sign_keypair();
-  const encryptingPublicKey = sodium.crypto_sign_ed25519_pk_to_curve25519(publicKey);
-  const encryptingSecretKey = sodium.crypto_sign_ed25519_sk_to_curve25519(secretKey);
-  const registrationSecret = Buffer.alloc(32);
+  const cryptoKeypair = sodium.crypto_box_keypair();
+  let registrationSecret = Buffer.alloc(32);
   sodium.randombytes(registrationSecret);
   return {
     signingPublicKey: b64.encode(publicKey),
     signingPrivateKey: b64.encode(secretKey),
-    encryptingPublicKey: b64.encode(encryptingPublicKey),
-    encryptingPrivateKey: b64.encode(encryptingSecretKey),
-    registrationSecret: b64.encode(registrationSecret),
+    encryptingPublicKey: b64.encode(cryptoKeypair.publicKey),
+    encryptingPrivateKey: b64.encode(cryptoKeypair.secretKey),
+    registrationSecret: registrationSecret.toString('hex')
   };
 };
