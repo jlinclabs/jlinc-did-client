d455183b5f5c7ac8d00d304f6114aacff2c83ccf
Victor Grey <victorgrey@users.noreply.github.com>
2018-11-05 22:27:09 -0800

    Should use the entity registration secret as the plaintext to secret.

--- jlinc-did/createRegistrantSecret.js
+++ jlinc-did/createRegistrantSecret.js
@@ -6,9 +6,9 @@ const b64 = require('urlsafe-base64');
 module.exports = function createRegistrantSecret(entity, serverPublicKey) {
   let nonce = Buffer.alloc(sodium.crypto_box_NONCEBYTES);
   sodium.randombytes(nonce);
-  let registrantSecret = this.createNonce();
+
   let cyphertext = sodium.crypto_box(
-    Buffer.from(registrantSecret),
+    Buffer.from(entity.registrationSecret),
     nonce,
     b64.decode(serverPublicKey),
     b64.decode(entity.encryptingPrivateKey)
