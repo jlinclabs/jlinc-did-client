474ddf78be35992ecaac145bc3049c957ea58fc2
Victor Grey <victorgrey@users.noreply.github.com>
2018-11-23 23:12:01 -0800

    Clean up includes.

--- jlinc-did/registerRequest.js
+++ jlinc-did/registerRequest.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// const sodium = require('sodium').api;
-// const b64 = require('urlsafe-base64');
 const request = require('request-promise');
 
 module.exports = async function registerRequest() {
