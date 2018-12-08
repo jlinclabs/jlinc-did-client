86ff5d1d1c37bfa55fb8d30dc84da041ad0c1a33
Victor Grey <victorgrey@users.noreply.github.com>
2018-12-07 20:23:48 -0800

    Method to revoke a DID.

---
+++ jlinc-did/revokeDID.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const jwt = require('jsonwebtoken');
+const request = require('request-promise');
+
+module.exports = async function revokeDID(id, registrationSecret) {
+  const url = this.Conf.didServerUrl;
+  const payload = {id};
+
+  //create the JWT
+  let token;
+  try {
+    token = jwt.sign(payload, registrationSecret, {algorithm: 'HS256'});
+  } catch (e) {
+    return e.message;
+  }
+  console.log(JSON.stringify({revokeRequest: token}));
+  try {
+    let options = {
+      method: 'POST',
+      uri: `${url}revoke`,
+      body: {revokeRequest: token},
+      json: true,
+      resolveWithFullResponse: true,
+      simple: false
+    };
+
+    let response = await request(options);
+    if (response.statusCode === 200) {
+      return {success: true, revoked: response.body.revoked};
+    } else {
+      return {success: false, status: response.statusCode, id, error: response.body.error};
+    }
+  } catch (e) {
+    return e.message;
+  }
+
+};
