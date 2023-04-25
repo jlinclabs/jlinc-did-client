67638e97693833aaf22cda8b76ea704ce4dd5fdd
Jared Grippe <jared@deadlyicon.com>
2023-04-25 16:20:25 -0700

    replaced sodium with sodium-native

--- jlinc-did/createKeys.js
+++ jlinc-did/createKeys.js
@@ -1,15 +1,19 @@
 'use strict';
 
 const b64 = require('urlsafe-base64');
-const sodium = require('sodium').api;
+const sodium = require('sodium-native');
 
 module.exports = function createKeys(){
-  const { publicKey, secretKey } = sodium.crypto_sign_keypair();
-  const cryptoKeypair = sodium.crypto_box_keypair();
+  const signingPublicKey = Buffer.alloc(sodium.crypto_sign_PUBLICKEYBYTES);
+  const signingPrivateKey = Buffer.alloc(sodium.crypto_sign_SECRETKEYBYTES);
+  sodium.crypto_sign_keypair(signingPublicKey, signingPrivateKey);
+  const encryptingPublicKey = Buffer.alloc(sodium.crypto_box_PUBLICKEYBYTES);
+  const encryptingPrivateKey = Buffer.alloc(sodium.crypto_box_SECRETKEYBYTES);
+  sodium.crypto_box_keypair(encryptingPublicKey, encryptingPrivateKey);
   return {
-    signingPublicKey: b64.encode(publicKey),
-    signingPrivateKey: b64.encode(secretKey),
-    encryptingPublicKey: b64.encode(cryptoKeypair.publicKey),
-    encryptingPrivateKey: b64.encode(cryptoKeypair.secretKey),
+    signingPublicKey: b64.encode(signingPublicKey),
+    signingPrivateKey: b64.encode(signingPrivateKey),
+    encryptingPublicKey: b64.encode(encryptingPublicKey),
+    encryptingPrivateKey: b64.encode(encryptingPrivateKey),
   };
 };
--- test/jlinc-did/verifySigningKeypair.spec.js
+++ test/jlinc-did/verifySigningKeypair.spec.js
@@ -1,27 +1,22 @@
 'use strict';
 
-const sodium = require('sodium').api;
-const b64 = require('urlsafe-base64');
-
+const createKeys = require('../../jlinc-did/createKeys');
 const verifySigningKeypair = require('../../jlinc-did/verifySigningKeypair');
 
 describe('verifySigningKeypair', function() {
 
   context('when given a valid signing key pair', function(){
     it('should return true', function(){
-      const { publicKey, secretKey } = sodium.crypto_sign_keypair();
+      const { signingPublicKey, signingPrivateKey } = createKeys();
       expect(
-        verifySigningKeypair({
-          signingPublicKey: b64.encode(publicKey),
-          signingPrivateKey: b64.encode(secretKey),
-        })
+        verifySigningKeypair({ signingPublicKey, signingPrivateKey })
       ).to.be.true;
     });
   });
 
   context('when given invalid arguments', function() {
     it('should throw an error', function() {
-      const { publicKey, secretKey } = sodium.crypto_sign_keypair();
+      const { signingPublicKey, signingPrivateKey } = createKeys();
       expect(() => {
         verifySigningKeypair({});
       }).to.throw('signingPublicKey is required');
@@ -35,23 +30,20 @@ describe('verifySigningKeypair', function() {
         verifySigningKeypair({ signingPublicKey: '123', signingPrivateKey: {} });
       }).to.throw('signingPrivateKey must of type string');
       expect(() => {
-        verifySigningKeypair({ signingPublicKey: b64.encode(publicKey), signingPrivateKey: '123' });
-      }).to.throw('argument secretKey must be (32U + 32U) bytes long');
+        verifySigningKeypair({ signingPublicKey, signingPrivateKey: '123' });
+      }).to.throw('"sk" must be crypto_sign_SECRETKEYBYTES bytes long');
       expect(() => {
-        verifySigningKeypair({ signingPublicKey: '123', signingPrivateKey: b64.encode(secretKey) });
-      }).to.throw('argument publicKey must be 32U bytes long');
+        verifySigningKeypair({ signingPublicKey: '123', signingPrivateKey });
+      }).to.throw('"pk" must be crypto_sign_PUBLICKEYBYTES bytes long');
     });
   });
 
   context('when given an invalid signing key pair', function(){
     it('should return false', function(){
-      const { publicKey: signingPublicKey } = sodium.crypto_sign_keypair();
-      const { secretKey: signingPrivateKey } = sodium.crypto_sign_keypair();
+      const { signingPublicKey } = createKeys();
+      const { signingPrivateKey } = createKeys();
       expect(
-        () => verifySigningKeypair({
-          signingPublicKey: b64.encode(signingPublicKey),
-          signingPrivateKey: b64.encode(signingPrivateKey),
-        })
+        () => verifySigningKeypair({ signingPublicKey, signingPrivateKey })
       ).to.throw('invalid keypair: failed to decode signed item');
     });
   });
